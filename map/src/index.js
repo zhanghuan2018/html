a6d2917f6bdf860e749ae015ad348202aac068db
zhangwenhuan <zhanghuan_2018@aliyun.com>
2019-01-30 17:47:13 +0800

    修改

--- map/src/index.js
+++ map/src/index.js
@@ -5,4 +5,12 @@ function component() {
   return element;
 }
 document.body.appendChild(component());
+
+/** 地图 */
 var map = new AMap.Map('container', {resizeEnable: true,zoom:11});
+AMap.plugin(['AMap.ToolBar','AMap.Scale'],function(){
+  /** 显示 比例尺寸 和 简易缩放按钮 */
+  var scale = new AMap.Scale();/** 比例尺寸  */
+  map.addControl(scale);
+  map.addControl(new AMap.ToolBar({liteStyle: true}));
+});
